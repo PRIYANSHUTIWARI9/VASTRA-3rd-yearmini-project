799e792ad3f915a86a625594334677f8309d3c2a
PRIYANSHUTIWARI9 <priyanshu.tiwari_cs18@gla.ac.in>
2021-04-21 23:34:01 +0530

    login_submit.php

---
+++ main/login_submit.php
@@ -0,0 +1,19 @@
+<?php
+require('connection.inc.php');
+require('functions.inc.php');
+
+$email=get_safe_value($con,$_POST['email']);
+$password=get_safe_value($con,$_POST['password']);
+
+$res=mysqli_query($con,"select * from users where email='$email' and password='$password'");
+$check_user=mysqli_num_rows($res);
+if($check_user>0){
+	$row=mysqli_fetch_assoc($res);
+	$_SESSION['USER_LOGIN']='yes';
+	$_SESSION['USER_ID']=$row['id'];
+	$_SESSION['USER_NAME']=$row['name'];
+	echo "valid";
+}else{
+	echo "wrong";
+}
+?>
