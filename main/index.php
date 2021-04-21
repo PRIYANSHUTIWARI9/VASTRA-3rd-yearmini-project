9e4a8b7a89c0431c705f82edf087f7ba52cb64b2
PRIYANSHUTIWARI9 <priyanshu.tiwari_cs18@gla.ac.in>
2021-04-21 23:32:01 +0530

    index.php

---
+++ main/index.php
@@ -0,0 +1,228 @@
+<?php require('top.php')?>
+<div class="body__overlay"></div>
+        
+        <!-- Start Slider Area -->
+        <div class="slider__container slider--one bg__cat--3">
+            <div class="slide__container slider__activation__wrap owl-carousel">
+                <!-- Start Single Slide -->
+                <div class="single__slide animation__style01 slider__fixed--height">
+                    <div class="container">
+                        <div class="row align-items__center">
+                            <div class="col-md-7 col-sm-7 col-xs-12 col-lg-6">
+                                <div class="slide">
+                                    <div class="slider__inner">
+                                        <h2>Iconic Fashion </h2>
+                                        <h1>Sale</h1>
+                                        <div class="cr__btn">
+                                            <a href="cart.html">Shop Now</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-sm-5 col-xs-12 col-md-5">
+                                <div class="slide__thumb">
+                                    <img src="images/slider/fornt-img/5.png" alt="slider images">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End Single Slide -->
+                <!-- Start Single Slide -->
+                <div class="single__slide animation__style01 slider__fixed--height">
+                    <div class="container">
+                        <div class="row align-items__center">
+                            <div class="col-md-7 col-sm-7 col-xs-12 col-lg-6">
+                                <div class="slide">
+                                    <div class="slider__inner">
+                                        <h2>collection 2021</h2>
+                                        <h1>SUMMER</h1>
+                                        <div class="cr__btn">
+                                            <a href="cart.html">Shop Now</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-sm-5 col-xs-12 col-md-5">
+                                <div class="slide__thumb">
+                                    <img src="images/slider/fornt-img/3.png" alt="slider images">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End Single Slide -->
+            </div>
+        </div>
+        <!-- Start Slider Area -->
+        <!-- Start Category Area -->
+        <section class="htc__category__area ptb--100">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="section__title--2 text-center">
+                            <h2 class="title__line">New Arrivals</h2>
+                            <p>But I must explain to you how all this mistaken idea</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="htc__product__container">
+                    <div class="row">
+                        <div class="product__list clearfix mt--30">
+							<?php
+							$get_product=get_product($con,4);
+							foreach($get_product as $list){
+							?>
+                            <!-- Start Single Category -->
+                            <div class="col-md-4 col-lg-3 col-sm-4 col-xs-12">
+                                <div class="category">
+                                    <div class="ht__cat__thumb">
+                                        <a href="product.php?id=<?php echo $list['id']?>">
+                                            <img src="<?php echo PRODUCT_IMAGE_SITE_PATH.$list['image']?>" alt="product images">
+                                        </a>
+                                    </div>
+                                    
+                                    <div class="fr__product__inner">
+                                        <h4><a href="product-details.html"><?php echo $list['name']?></a></h4>
+                                        <ul class="fr__pro__prize">
+                                            <li class="old__prize"><?php echo $list['mrp']?></li>
+                                            <li><?php echo $list['price']?></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- End Single Category -->
+							<?php } ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End Category Area -->
+        <!-- Start Product Area -->
+        <section class="ftr__product__area ptb--100">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="section__title--2 text-center">
+                            <h2 class="title__line">Best Seller</h2>
+                            <p>But I must explain to you how all this mistaken idea</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="product__wrap clearfix">
+                        <!-- Start Single Category -->
+                        <div class="col-md-4 col-lg-3 col-sm-4 col-xs-12">
+                            <div class="category">
+                                <div class="ht__cat__thumb">
+                                    <a href="product-details.html">
+                                        <img src="images/product/campus jeans_.png" alt="product images">
+                                    </a>
+                                </div>
+                                <div class="fr__hover__info">
+                                    <ul class="product__action">
+                                        <li><a href="wishlist.html"><i class="icon-heart icons"></i></a></li>
+
+                                        <li><a href="cart.html"><i class="icon-handbag icons"></i></a></li>
+
+                                        <li><a href="#"><i class="icon-shuffle icons"></i></a></li>
+                                    </ul>
+                                </div>
+                                <div class="fr__product__inner">
+                                    <h4><a href="product-details.html">Levi's Jeans</a></h4>
+                                    <ul class="fr__pro__prize">
+                                        <li class="old__prize">Rs. 3000</li>
+                                        <li>Rs. 2500</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End Single Category -->
+                        <!-- Start Single Category -->
+                        <div class="col-md-4 col-lg-3 col-sm-4 col-xs-12">
+                            <div class="category">
+                                <div class="ht__cat__thumb">
+                                    <a href="product-details.html">
+                                        <img src="images/product/superdry t-shirt.png" alt="product images">
+                                    </a>
+                                </div>
+                                <div class="fr__hover__info">
+                                    <ul class="product__action">
+                                        <li><a href="wishlist.html"><i class="icon-heart icons"></i></a></li>
+
+                                        <li><a href="cart.html"><i class="icon-handbag icons"></i></a></li>
+
+                                        <li><a href="#"><i class="icon-shuffle icons"></i></a></li>
+                                    </ul>
+                                </div>
+                                <div class="fr__product__inner">
+                                    <h4><a href="product-details.html">Super Dry T-Shirt</a></h4>
+                                    <ul class="fr__pro__prize">
+                                        <li class="old__prize">Rs. 2999</li>
+                                        <li>Rs. 1999</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End Single Category -->
+                        <!-- Start Single Category -->
+                        <div class="col-md-4 col-lg-3 col-sm-4 col-xs-12">
+                            <div class="category">
+                                <div class="ht__cat__thumb">
+                                    <a href="product-details.html">
+                                        <img src="images/product/calvin_klein t-shirt.png" alt="product images">
+                                    </a>
+                                </div>
+                                <div class="fr__hover__info">
+                                    <ul class="product__action">
+                                        <li><a href="wishlist.html"><i class="icon-heart icons"></i></a></li>
+
+                                        <li><a href="cart.html"><i class="icon-handbag icons"></i></a></li>
+
+                                        <li><a href="#"><i class="icon-shuffle icons"></i></a></li>
+                                    </ul>
+                                </div>
+                                <div class="fr__product__inner">
+                                    <h4><a href="product-details.html">Calvin Klien T-Shirt</a></h4>
+                                    <ul class="fr__pro__prize">
+                                        <li class="old__prize">Rs. 2599</li>
+                                        <li>Rs. 2199</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End Single Category -->
+                        <!-- Start Single Category -->
+                        <div class="col-md-4 col-lg-3 col-sm-4 col-xs-12">
+                            <div class="category">
+                                <div class="ht__cat__thumb">
+                                    <a href="product-details.html">
+                                        <img src="images/product/kid t-shirt.png" alt="product images">
+                                    </a>
+                                </div>
+                                <div class="fr__hover__info">
+                                    <ul class="product__action">
+                                        <li><a href="wishlist.html"><i class="icon-heart icons"></i></a></li>
+
+                                        <li><a href="cart.html"><i class="icon-handbag icons"></i></a></li>
+
+                                        <li><a href="#"><i class="icon-shuffle icons"></i></a></li>
+                                    </ul>
+                                </div>
+                                <div class="fr__product__inner">
+                                    <h4><a href="product-details.html">USPA T-Shirt</a></h4>
+                                    <ul class="fr__pro__prize">
+                                        <li class="old__prize">Rs. 1600</li>
+                                        <li>Rs. 1199</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End Single Category -->
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End Product Area -->
+<?php require('footer.php')?>        
